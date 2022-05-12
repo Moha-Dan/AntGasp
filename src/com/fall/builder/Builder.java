c404f7d6cefa102e7acb472e33286998c150d75b
Moha-Dan <73938112+Moha-Dan@users.noreply.github.com>
2022-05-12 12:29:38 +0200

    ObjectBuilder

---
+++ src/com/fall/builder/Builder.java
@@ -0,0 +1,6 @@
+package com.fall.builder;
+
+public abstract class Builder {
+	public abstract String buildGroup(Object oj);
+	public abstract String build(Object oj);
+}
---
+++ src/com/fall/builder/ObjectBuilder.java
@@ -0,0 +1,27 @@
+package com.fall.builder;
+
+import java.util.List;
+
+import com.fall.persistence.ParseClass;
+
+public class ObjectBuilder{
+	public static String buildGroup(Object obj){
+		List<String> ls = ParseClass.getProprietyNames(obj);
+		StringBuffer form = new StringBuffer();
+		System.out.print(ls);
+		form.append("<div class=\"col-10 col-lg-6 centred col\">");
+		for(String prop : ls) {
+			form.append("<div class=\"col-lg-10 col centred\">");
+			Object value = ParseClass.getPropriety(obj, prop);
+			String STRvalue = "";
+			form.append("<label class=\"form-label\">"+prop+"</label>");
+			if(value!=null)
+				STRvalue = value.toString();
+			form.append("<p class=\"form-control\" >"+STRvalue+"</p>");
+			form.append("</div>");
+		}
+		form.append("</div>");
+		System.out.println(form);
+		return form.toString();
+	}
+}
